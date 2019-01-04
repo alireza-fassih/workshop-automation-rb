ec7ce8619e1a10cf0d069fc1cb89e16d55acaaf3
alireza.fassih <fassih.alireza@gmail.com>
2019-01-04 14:18:28 +0330

    make app run

--- workshop-core/src/main/java/ir/fassih/workshop/core/WorkshopCoreAutoConfiguration.java
+++ workshop-core/src/main/java/ir/fassih/workshop/core/WorkshopCoreAutoConfiguration.java
@@ -1,10 +1,28 @@
 package ir.fassih.workshop.core;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import ir.fassih.workshop.core.localeutil.LocaleProperties;
+import ir.fassih.workshop.core.localeutil.LocaleUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan("ir.fassih.workshop.core")
+@Configuration
 public class WorkshopCoreAutoConfiguration {
 
+    @ConditionalOnMissingBean(LocaleUtil.class)
+    @EnableConfigurationProperties(LocaleProperties.class)
+    @AllArgsConstructor(onConstructor = @__(@Autowired))
+    protected static class LocalUtilAutoConfiguration {
+
+        private final LocaleProperties properties;
+
+        @Bean
+        public LocaleUtil localeUtil() {
+            return new LocaleUtil(properties);
+        }
+
+    }
 }
--- workshop-core/src/main/java/ir/fassih/workshop/core/localeutil/LocaleProperties.java
+++ workshop-core/src/main/java/ir/fassih/workshop/core/localeutil/LocaleProperties.java
@@ -4,12 +4,10 @@ package ir.fassih.workshop.core.localeutil;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter @Setter
-@Configuration
 @ConfigurationProperties(prefix = "workshop.locale")
-class LocaleProperties {
+public class LocaleProperties {
 
 
     private String propertiesName = "classpath*:/*-locale-msg.properties";
