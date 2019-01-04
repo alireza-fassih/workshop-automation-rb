340026f529d6d827dbe780fb3550aabd2c53c642
alireza.fassih <fassih.alireza@gmail.com>
2019-01-04 15:24:34 +0330

    add vueitfy and vueitfy rtl to project

---
+++ workshop-ui/src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+import 'vuetify-rtl-style/dist/css/vuetify-rtl-style.min.css'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+})
