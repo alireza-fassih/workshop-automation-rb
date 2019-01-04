33d11d315434abb3ac118472a4a143f18a9fe336
alireza.fassih <fassih.alireza@gmail.com>
2019-01-04 12:50:50 +0330

    init porject

---
+++ src/main/java/ir/fassih/workshopautomationrb/WorkshopAutomationRbApplication.java
@@ -0,0 +1,14 @@
+package ir.fassih.workshopautomationrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopAutomationRbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopAutomationRbApplication.class, args);
+	}
+
+}
+
