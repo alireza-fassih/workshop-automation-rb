daaf122aa384440f9366ec6716861b73f58d04a2
alireza.fassih <fassih.alireza@gmail.com>
2019-01-04 13:07:46 +0330

    adding core module to project

---
+++ workshop-core/src/main/java/ir/fassih/workshop/core/WorkshopCoreAutoConfiguration.java
@@ -0,0 +1,10 @@
+package ir.fassih.workshop.core;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan("ir.fassih.workshop.core")
+public class WorkshopCoreAutoConfiguration {
+
+}
