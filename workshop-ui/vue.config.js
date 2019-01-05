a1269763b31ac12f62f20a2929b521c07c1f0c82
alireza.fassih <fassih.alireza@gmail.com>
2019-01-05 22:48:31 +0330

    add app table

--- workshop-ui/vue.config.js
+++ workshop-ui/vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
         port: 3000,
         proxy: [{
             path: '^/rest/*',
+            headers: {
+                'Workshop-app': 'demo'
+            },
             target: 'http://localhost:8080'
         }]
     }
