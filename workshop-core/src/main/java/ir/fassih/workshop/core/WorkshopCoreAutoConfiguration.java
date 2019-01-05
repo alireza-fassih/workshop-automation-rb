d3c78d572c16e50d042721ac693bf6e9589996a5
alireza.fassih <fassih.alireza@gmail.com>
2019-01-05 23:21:47 +0330

    enable jpa repository

--- workshop-core/src/main/java/ir/fassih/workshop/core/WorkshopCoreAutoConfiguration.java
+++ workshop-core/src/main/java/ir/fassih/workshop/core/WorkshopCoreAutoConfiguration.java
@@ -7,14 +7,18 @@ import lombok.AllArgsConstructor;
 import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EntityScan(basePackages = "ir.fassih.workshop.core.entity")
+@EnableJpaRepositories(basePackages = "ir.fassih.workshop.core.repository")
 @Import(CommonsExceptionHandler.class)
 public class WorkshopCoreAutoConfiguration {
 
---
+++ workshop-core/src/main/java/ir/fassih/workshop/core/repository/WorkshopAppRepository.java
@@ -0,0 +1,9 @@
+package ir.fassih.workshop.core.repository;
+
+import ir.fassih.workshop.core.entity.WorkshopAppEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkshopAppRepository extends JpaRepository<WorkshopAppEntity, Long> {
+}
